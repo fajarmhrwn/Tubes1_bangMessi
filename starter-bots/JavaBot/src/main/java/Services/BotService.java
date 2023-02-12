94705fe692cdf441cd2e039e44bc49f978ee0607
farhan <mohdfarhanfahrezy@gmail.com>
2023-02-13 00:26:45 +0700

    adding function to avoid gascloud (experimental)

--- starter-bots/JavaBot/src/main/java/Services/BotService.java
+++ starter-bots/JavaBot/src/main/java/Services/BotService.java
@@ -41,33 +41,45 @@ public class BotService {
         if (!gameState.getGameObjects().isEmpty()) {
             // Mencari makanan terdekat
             var foodList = gameState.getGameObjects()
-                    .stream().filter(item -> item.getGameObjectType() == ObjectTypes.FOOD)
+                    .stream().filter(item -> item.getGameObjectType() == ObjectTypes.FOOD || item.getGameObjectType() == ObjectTypes.SUPER_FOOD)
                     .sorted(Comparator
                             .comparing(item -> getDistanceBetween(bot, item)))
                     .collect(Collectors.toList());
+            // Mencari gas cloud terdekat
+            var gasCloudList = gameState.getGameObjects()
+                    .stream().filter(gas -> gas.getGameObjectType() == ObjectTypes.GAS_CLOUD)
+                    .sorted(Comparator
+                            .comparing(gas -> getDistanceBetweenWithSize(bot, gas)))
+                    .collect(Collectors.toList());
             // Mencari player terdekat
             var playerList = gameState.getPlayerGameObjects()
-                    .stream().filter(item -> item.getGameObjectType() == ObjectTypes.PLAYER)
+                    .stream().filter(player -> player.getGameObjectType() == ObjectTypes.PLAYER)
                     .sorted(Comparator
-                        .comparing(item -> getDistanceBetween(bot, item)))
+                        .comparing(player -> getDistanceBetweenWithSize(bot, player)))
                     .collect(Collectors.toList());
+
             int enemySize = playerList.get(1).getSize();
             int mySize = bot.getSize();
 
-            if (getDistanceBetween(bot, playerList.get(1)) <= 2 && enemySize > mySize){
+            if (getDistanceBetween(bot, playerList.get(1)) <= getSaveDistance(playerList.get(1)) && enemySize > mySize){
                 // conditional kalau ada enemy yang deket dan lebih besar
-                playerAction.heading = (getHeadingBetween(playerList.get(1)) + 180) % 360;
+                playerAction.heading = getHeadingAvoid(playerList.get(1));
             }
-            else if (getDistanceFromBorder() <= 2){
-                // ketika terlalu mepet ujung 
-                playerAction.heading = getHeadingAvoidBorder();
-            }
-            else if(mySize > enemySize && bot.getSize() >= 30){
+            else if(mySize > enemySize && bot.getSize() >= 200){
                 // kalau ada yg bisa dimakan
+                playerAction.action = PlayerActions.START_AFTERBURNER;
                 playerAction.heading = getHeadingBetween(playerList.get(1));
             } else {
                 // makan
                 playerAction.heading = getHeadingBetween(foodList.get(0));
+            }
+
+            if (getDistanceFromBorder() <= 400 ){
+                // ketika terlalu mepet ujung 
+                playerAction.heading = getHeadingFrom2(playerAction.heading, getHeadingAvoidBorder());
+            }
+            if (getDistanceBetweenWithSize(bot, gasCloudList.get(0)) <= 50 && isGonnaCrash(gasCloudList.get(0), playerAction.heading)){
+                playerAction.heading = getHeadingAvoidObject(gasCloudList.get(0),playerAction.heading);
             }
         }
             
@@ -93,7 +105,11 @@ public class BotService {
     private double getDistanceBetween(GameObject object1, GameObject object2) {
         var triangleX = Math.abs(object1.getPosition().x - object2.getPosition().x);
         var triangleY = Math.abs(object1.getPosition().y - object2.getPosition().y);
-        return Math.sqrt(triangleX * triangleX + triangleY * triangleY) - (object1.getSize()+object2.getSize())/2;
+        return Math.sqrt(triangleX * triangleX + triangleY * triangleY);
+    }
+
+    private double getDistanceBetweenWithSize(GameObject object1, GameObject object2) {
+        return getDistanceBetween(object1, object2) - ((object1.getSize()+object2.getSize())/2);
     }
 
     public double getDistanceFromBorder(){
@@ -108,14 +124,43 @@ public class BotService {
         return (direction + 360) % 360;
     }
 
+    private int getHeadingAvoid(GameObject otherObject) {
+        return (getHeadingBetween(otherObject) + 180) % 360;
+    }
+
     private int getHeadingAvoidBorder(){
         Position position = new Position(0, 0);
         GameObject center = new GameObject(null, null, null, null, position, null, null, null, null, null, null);
         return getHeadingBetween(center);
     }
 
+    private int getHeadingFrom2(int heading1, int heading2){
+        int min = Math.min(heading1, heading2);
+        return (min + (heading2-heading1)/2 + 360) % 360;
+    }
+
     private int toDegrees(double v) {
         return (int) (v * (180 / Math.PI));
+    }
+
+    private int getSaveDistance(GameObject otherPlayer){
+        int speedMax = 2*otherPlayer.getSpeed() - bot.getSpeed();
+        return 10*speedMax;
+    }
+
+    private boolean isGonnaCrash(GameObject otherObject, int heading){
+        int theta = toDegrees(Math.atan2((bot.getSize() + otherObject.getSize())/2, getDistanceBetween(bot, otherObject)));
+        return Math.abs(heading-getHeadingBetween(otherObject)) <= theta;
+    }
+
+    private int getHeadingAvoidObject(GameObject otherObject, int heading){
+        int theta = toDegrees(Math.atan2(otherObject.getSize()/2, getDistanceBetween(bot, otherObject)));
+        int tilt = theta - Math.abs(heading-getHeadingBetween(otherObject));
+        if (heading >= getHeadingBetween(otherObject)){
+            return heading+tilt;
+        } else {
+            return heading-tilt;
+        }
     }
 
 }
